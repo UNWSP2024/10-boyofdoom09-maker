56754429a586bf1ada8e02bdad7fe138ca0ce663
boyofdoom09-maker <boyofdoom09@outlook.com>
2025-11-09 08:23:49 -0600

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -8,3 +8,19 @@
 # Mark Jones	39119	IT	Programmer
 # Joy Rogers	81774	Manufacturing	Engineer
 # The program should store the data in the three objects, then display the data for each employee on the screen.
+class Employee:
+    def __init__(self, name, id_number, department, job_title):
+        self.name = name
+        self.id_number = id_number
+        self.department = department
+        self.job_title = job_title
+
+
+employee1 = Employee("Susan Meyers", 47899, "Accounting", "Vice President")
+employee2 = Employee("Mark Jones", 39119, "IT", "Programmer")
+employee3 = Employee("Joy Rogers", 81774, "Manufacturing", "Engineer")
+
+employees = [employee1, employee2, employee3]
+
+for employee in employees:
+    print(f"Name: {employee.name}, ID Number: {employee.id_number}, Department: {employee.department}, Job Title: {employee.job_title}")
